5146412043c06756352a0380c126442519965fe5
venera-gibaeva <98085115+venera-gibaeva@users.noreply.github.com>
2022-04-06 14:14:44 +0300

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from config import keys
+
+class ConvertionExeption(Exception):
+    pass
+
+class Converter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+           raise ConvertionExeption(f'Невозможно перевести одинаковые валюты{base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionExeption(f'Не удалось обработать валюту {quote}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionExeption(f'Не удалось обработать валюту {base}')
+        try:
+            amount = int(amount)
+        except ValueError:
+            raise ConvertionExeption(f'Не удалось обработать количество {amount}')
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_base = float(json.loads(r.content)[keys[quote]])
+        return total_base * amount
